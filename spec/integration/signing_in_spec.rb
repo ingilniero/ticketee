e6951d8f207ec22c38e1721cf46e4d4a68aae846
Gilberto Villa <ingilniero@gmail.com>
2013-03-01 16:41:25 -0600

    Add feature for signing in via the Decise-provided form

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -2,13 +2,23 @@ require 'spec_helper'
 
 feature 'Signing in' do
 	before do
-		Factory(:user, :email => 'ticketee@example.com')
+		Factory(:user, :email => 'example@ticketee.com')
 	end
 
 	scenario 'Signing in via confirmation' do
-		open_email 'ticketee@example.com', :with_subject => /Confirmation/
+		open_email 'example@ticketee.com', :with_subject => /Confirmation/
 		click_first_link_in_email
 		page.should have_content('Your account was successfully confirmed')
-		page.should have_content('Signed in as ticketee@example.com')
+		page.should have_content('Signed in as example@ticketee.com')
+	end
+
+	scenario 'Signing in via form' do
+		User.find_by_email('example@ticketee.com').confirm!
+		visit '/'
+		click_link 'Sign in'
+		fill_in 'Email', :with => 'example@ticketee.com'
+		fill_in 'Password', :with => 'password'
+		click_button 'Sign in'
+		page.should have_content('Signed in successfully.')
 	end
 end
