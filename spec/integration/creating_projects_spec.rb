c66abb0a2c74ad4a491bb1c1a48c0787a7896ba1
Gilberto Villa <ingilniero@gmail.com>
2013-02-28 22:12:58 -0600

    Add validation to esure names are specified when creating projects

--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
+	# before do
+	# 	visit '/'
+	# 	click_link 'New Project'
+	# end
+		
 	scenario 'can create a project' do
 		visit '/'
 		click_link 'New Project'
@@ -13,5 +18,13 @@ feature 'Creating Projects' do
 		page.current_url.should == project_url(project)
 		title = 'Gedit - Projects - Ticketee'
 		find('title').should have_content(title)
+	end
+
+	scenario 'can not create a project without a name' do
+		visit '/'
+		click_link 'New Project'
+		click_button 'Create Project'
+		page.should have_content('Project has not been created.')
+		page.should have_content("Name can't be blank")
 	end
 end
