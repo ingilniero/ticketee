4cfeba5ec13295a4912751b552ac9d1092720f90
Gilberto Villa <ingilniero@gmail.com>
2013-03-01 11:42:35 -0600

    Implement features for displaying a list of relevant tickets for projects and viewing particular tickets

---
+++ spec/integration/viewing_tickets_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+feature 'Viewing tickets' do
+	before do
+		gedit = Factory(:project, :name => 'Gedit')
+		Factory(:ticket,
+						:project => gedit,
+						:title => 'Make it shiny!',
+						:description => 'Gradients! Starbursts! Oh my!')
+
+		internet_explorer = Factory(:project, :name => 'Internet Explorer')
+		Factory(:ticket,
+						:project => internet_explorer,
+						:title => 'Standards compliance',
+						:description => "Isn't a joke.")
+		visit '/'
+	end
+
+	scenario 'Viewing tickets for a given project' do
+		click_link 'Gedit'
+		page.should have_content('Make it shiny!')
+		page.should_not have_content('Standards compliance')
+
+		click_link 'Make it shiny!'
+		within('#ticket h2') do
+			page.should have_content('Make it shiny!')
+		end
+		page.should have_content('Gradients! Starbursts! Oh my!')
+	end
+end
