73539d471c5b730b5592daaaa7e1449801ee8702
Gilberto Villa <ingilniero@gmail.com>
2013-03-03 11:29:07 -0600

    Add feature for deleting users, including protection against self-deletion

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -31,7 +31,7 @@ class Admin::UsersController < Admin::BaseController
       params[:user].delete(:password)
       params[:user].delete(:password_confirmation)
     end
-    
+
     if @user.update_attributes(params[:user], :as => :admin)
       flash[:notice] = "User has been updated."
       redirect_to admin_users_path
@@ -39,6 +39,16 @@ class Admin::UsersController < Admin::BaseController
       flash[:alert] = "User has not been updated."
       render :action => 'edit'
     end
+  end
+
+  def destroy
+    if @user == current_user
+      flash[:alert] = "You cannot delete yourself!"
+    else
+      @user.destroy
+      flash[:notice] = "User has been deleted."
+    end
+    redirect_to admin_users_path
   end
 
   private
